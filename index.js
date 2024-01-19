775ba03c6c1c7697cdc5d548aa08b1d150f795c5
SharpLad <143443228+yrezvani@users.noreply.github.com>
2024-01-19 19:15:21 +0000

    Add final comments to the code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Import required modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,9 +6,11 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// set output folder the HTML file
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// Import function to render the HTML file
 const render = require("./src/page-template.js");
 
 // Initialize an array to store team members
